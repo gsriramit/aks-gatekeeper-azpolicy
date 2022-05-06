172f3f5eed0dbb0d0ba89eaf95bf0e2ee84542bd
Sriram Ganesan <gsriramit@gmail.com>
2022-05-06 16:12:44 +0530

    added commands to verify violations

--- DeploymentScripts/deployAKSCluster.sh
+++ DeploymentScripts/deployAKSCluster.sh
@@ -36,6 +36,9 @@ az aks create -g $RESOURCEGROUP_NAME \
 --network-plugin $PLUGIN \
 --zones 1 2 3
 
+# Verify the presence and state of the gatekeeper pods
+kubectl get pods -n gatekeeper-system
+
 # Get the credentials to the working cluster
 az aks get-credentials --resource-group $RESOURCEGROUP_NAME --name $CLUSTER_NAME --admin
 
@@ -49,20 +52,25 @@ kubectl apply -f testspreadconstraintDeployment.yaml
 # check the placement of the pods across the zones
 kubectl get pods -n default -o wide
 
+########## Illustration of Pod Security Policies ################################
 # Get the definitionID of the Container Privilege Escalation prevention policy
-
 # Apply the policy to the resource group that this cluster is created in
 az policy assignment create --name 'do-not-allow-container-privilege-escalation' \
 --display-name 'Kubernetes clusters should not allow container privilege escalation' \
 --scope /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCEGROUP_NAME --policy $PREVENT_PRIVILEGEDPODS_POLICYNAME \
 -p "{ \"Effect\": \
     { \"value\": \"deny\" } }"
-# Create a test deployment that creates a pod/container with elevatedprivileges:true .This should fail
+# Create a test deployment that creates a pod/container with elevatedprivileges:true .This should fail with the appropriate error message
+#####  [azurepolicy-psp-container-no-privilege-esc-794edd6fbb66b2a39128] Privilege escalation container is not allowed: nginx-privileged #####
+# Note: Run the following commands from within ClusterManifests/TestDeployment/
 kubectl apply -f PrivilegedPod.yaml
 
 # Get the pod information (replace the pod's name with the actual value)
-kubectl get -o json pod <podplacementdeployment-6cdcc9585->
+# kubectl get -o json pod <podname>
+##############################################################################
 
+
+########## Illustration of Pod Reliability Policies ################################
 # Apply the Custom Policy that ensures the reliability of the workload pods by enforcing zone-redunandancy
 az policy assignment create --name 'mandate-pod-spread-constraints' \
 --display-name 'Kubernetes clusters should not allow pods without PodTopologySpreadConstraint' \
@@ -78,3 +86,24 @@ kubectl get constrainttemplates
 kubectl apply -f testNoSpreadconstraint.yaml
 # The deployment should get rejected with the appropriate violation error message
 # Error from server ([azurepolicy-prp-pod-spreadconstraint-manda-45f6680c714279da4e5b] At least 1 spread constraint needs to be defined: %!v(MISSING)): error when creating "testNoSpreadconstraint.yaml": admission webhook "validation.gatekeeper.sh" denied the request: [azurepolicy-prp-pod-spreadconstraint-manda-45f6680c714279da4e5b] At least 1 spread constraint needs to be defined: %!v(MISSING)
+
+## Fetch the constraint kind created 
+kubectl get K8sAzurePodSpreadConstraintsEnforced
+## NAME                                                              AGE
+# azurepolicy-prp-pod-spreadconstraint-manda-bce35ea75962704aa50c   26m
+
+## describe the specific instance of the constraint created to read the policy violations
+## Note: The violations array will be populated only if the Policy Action is set to "Audit" and not "Deny"
+# Replace the name of the constraint instance (specified in < >) with the value that you obtain in the previous step
+kubectl describe K8sAzurePodSpreadConstraintsEnforced <azurepolicy-prp-pod-spreadconstraint-manda-bce35ea75962704aa50c>
+
+######################################################
+#Total Violations:  1
+# Violations:
+#  Enforcement Action:  dryrun
+#  Kind:                Pod
+#  Message:             At least 1 spread constraint needs to be defined: mypod
+#  Name:                mypod
+#  Namespace:           default    
+
+##############################################################################
